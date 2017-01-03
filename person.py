001bd33712451337dbfdb1c867e1bd9ee54b121e
Michal <r.pr.michal.doniec@gmail.com>
2017-01-03 14:05:14 +0100

    Corrected error handling.

--- person.py
+++ person.py
@@ -1,14 +1,16 @@
 class Person:
 
 
-    def __init__(self,first_name = 'empty', last_name = 'empty',year_of_birth = 0, gender = 'empty',class_location = 'empty'):
+    def __init__(self,first_name = 'empty', last_name = 'empty',year_of_birth = 0, gender = 'empty', class_location = 'empty'):
 
         self.first_name = first_name
         self.last_name = last_name
         self.year_of_birth = year_of_birth
         self.gender = gender
+        self.class_location = class_location
 
-        if self.first_name == 'empty' or self.last_name == 'empty' or self.gender == 'empty' or year_of_birth == 0:
+        if self.first_name == 'empty' or self.last_name == 'empty' or self.class_location == 'empty' \
+                or self.gender == 'empty' or self.year_of_birth == 0:
             raise AttributeError('At least one of the attributes of Person class is empty. Please indicate first name '
             'last name, year of birth and gender.')
 
@@ -16,6 +18,3 @@ class Person:
 
         if self.gender.lower() not in genders:
             raise AttributeError('Gender attribute is incorrect (please indicate either male, female or notsure).')
-
-
-

7d57e8b3be00e3c2dbcd7d0029ea742d7da51332
kjaroska <kjaroska@gmail.com>
2017-01-03 15:05:27 +0100

    student #classmethod and ccclass making students

--- codecool_class.py
+++ codecool_class.py
@@ -1,6 +1,13 @@
 from mentor import Mentor
 from student import Student
 
+students_list = Student.create_by_csv("data/students.csv")
+students_obj_lst = []
+for human in students_list:
+    students_obj = Student(human[0], human[1], human[2], human[3], human[4], human[5], human[6], human[7])
+    students_obj_lst.append(sudents_obj)
+print (students_obj_lst)
+
 
 class CodecoolClass:
     pass
--- student.py
+++ student.py
@@ -1,5 +1,19 @@
 from person import Person
+import csv
 
 
 class Student(Person):
-    pass
+
+    @classmethod
+    def create_by_csv(cls, filename):
+        students_list = []
+        f = open(filename)
+        for student in csv.reader(f):
+            students_list.append(student)
+        return students_list
+
+    def __init__(self, first_name, last_name, date_of_birth, gender, class_location, class_annual, energy_level, knowledge_level):
+        Person.__init__(self, first_name, last_name, date_of_birth, gender, class_location)
+        self.class_annual = class_annual
+        self.energy_level = energy_level
+        self.knowledge_level = knowledge_level

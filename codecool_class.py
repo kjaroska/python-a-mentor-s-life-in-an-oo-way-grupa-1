778b30c8727ae823f13f447e0e90d95013069760
Michal <r.pr.michal.doniec@gmail.com>
2017-01-04 12:01:31 +0100

    Generate local method created.

--- codecool_class.py
+++ codecool_class.py
@@ -2,17 +2,21 @@ from mentor import Mentor
 from student import Student
 
 
+
 class CodecoolClass:
 
-    def __init__(self,location):
-        self.student_list = Student.create_by_csv()
-        self.location = location
-        self.year
+
+    def __init__(self):
+        self.students = Student.create_by_csv()
+        #self.mentors = 'X'
+        self.location = 'Cracow'
+        self.year = 2016
 
 
+    @classmethod
+    def generate_local(self):
+        local_class = CodecoolClass()
+        return local_class
 
 
-
-M = CodecoolClass()
-
-print(M)
+print(CodecoolClass().__dict__)

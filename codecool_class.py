4392318c8b4f8afc74f0a16d7dcd00c6fb8403f4
oskardz <oskardz1991@gmail.com>
2017-01-04 12:08:36 +0100

    find student def

--- codecool_class.py
+++ codecool_class.py
@@ -4,15 +4,21 @@ from student import Student
 
 class CodecoolClass:
 
-    def __init__(self,location):
+    def __init__(self):
         self.student_list = Student.create_by_csv()
-        self.location = location
-        self.year
+        self.mentors_list = Mentor.create_by_csv()
 
+    def find_student_by_full_name(self):
+        name_of_student = input("Please, give a name")
+        for student in self.student_list   :
+            if student.__dict__["first_name"] == name_of_student:
+                print(student.__dict__)
+            else:
+                print(student.__dict__)
+                print ("No such student, try again")
+                break
 
-
-
-
-M = CodecoolClass()
-
-print(M)
+m = CodecoolClass()
+# for mentor in m.mentors_list:
+#     print(mentor.__dict__)
+m.find_student_by_full_name()

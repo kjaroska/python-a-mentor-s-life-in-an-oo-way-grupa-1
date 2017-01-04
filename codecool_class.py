fc7c0100dba141390483162ee1f8bd39b160e71f
Michal <r.pr.michal.doniec@gmail.com>
2017-01-04 11:09:19 +0100

    Changes to student (create_csv meth), codecool class (init meth).

--- codecool_class.py
+++ codecool_class.py
@@ -1,14 +1,18 @@
 from mentor import Mentor
 from student import Student
-from person import Person
-
-students_list = Student.create_by_csv("data/students.csv")
-students_obj_lst = []
-for human in students_list:
-    students_obj = Student(human[0], human[1], int(human[2]), human[3], human[4], human[5], human[6], human[7])
-    students_obj_lst.append(students_obj.__dict__)
-print (students_obj_lst)
 
 
 class CodecoolClass:
-    pass
+
+    def __init__(self,location):
+        self.student_list = Student.create_by_csv()
+        self.location = location
+        self.year
+
+
+
+
+
+M = CodecoolClass()
+
+print(M)
--- student.py
+++ student.py
@@ -5,12 +5,18 @@ import csv
 class Student(Person):
 
     @classmethod
-    def create_by_csv(cls, filename):
+    def create_by_csv(cls, filename = 'data/students.csv'):
         students_list = []
         f = open(filename)
         for student in csv.reader(f):
             students_list.append(student)
-        return students_list
+
+        students_obj_lst= []
+        for human in students_list:
+            students_obj = Student(human[0], human[1], int(human[2]), human[3], human[4], human[5], human[6],
+                                       human[7])
+            students_obj_lst.append(students_obj)
+        return students_obj_lst
 
     def __init__(self, first_name, last_name, date_of_birth, gender, class_location, class_annual, energy_level, knowledge_level):
         Person.__init__(self, first_name, last_name, date_of_birth, gender, class_location)
